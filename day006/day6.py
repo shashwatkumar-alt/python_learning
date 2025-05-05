90aec87ad32db20432f1850c48d39ed6c5ff7dc1
shashwatkumar-alt <shashwatsk1802@gmail.com>
2025-05-05 11:19:29 +0530

    day6

---
+++ day006/day6.py
@@ -0,0 +1,14 @@
+a = "nothing"
+b = "just do it"
+print(a+b)
+print(type(a))
+print(type(b))
+
+list1 = [1,22,3.14,["name","shashwat"],["apple","banana"]]
+print(list1)
+
+tuple1 = (("name","shashwat"),("learning","python"))
+print(tuple1)
+
+dict1 = {"name":"Shashwat","learning":"python","can complete playlist today":1}
+print(dict1)
